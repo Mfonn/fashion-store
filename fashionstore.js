18e5ae3c5802093355798651a3f394a85e2830a3
Mfonn <67243684+Mfonn@users.noreply.github.com>
2020-07-16 13:40:47 +0100

    Add files via upload

---
+++ fashionstore.js
@@ -0,0 +1,7 @@
+function,nb ,m,m  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function.bj k closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
